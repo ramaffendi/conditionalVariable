724b4e682e0caa6cd55c699b1bc81e9b1b1d533b
RAMADHAN EFFENDI <ramadhaneffendi98@gmail.com>
2023-10-16 21:44:26 +0700

    kondisiVariable

---
+++ OneDrive/Desktop/Ramadhan_freeclass_eduwork/Tugas_html/conditional variable/conditionalvariable.js
@@ -0,0 +1,58 @@
+//Penggunaan operator ternary
+// const nilai = 75;
+// const kelulusan = nilai >= 70 || nilai <= 70 ? "Lulus" : "Tidak lulus";
+// alert(kelulusan) = "selamat anda lulus"
+
+const nilai2 = 61;
+const kelulusan2 = nilai2 >= 80 && nilai2 <= 100 ? "true" : "false";
+console.log(kelulusan2) ;
+
+// // kondisi variabel
+// const umur = 60;
+// const punyaSIM = true;
+// const punyaMobil = false;
+
+// const keterangan = umur > 50 && punyaSIM || punyaMobil ? "Boleh mengemudi" : "Tidak boleh mengemudi";
+// alert(keterangan); // Boleh mengemudi
+
+/*
+const dataKaryawan = [
+    {
+      nama: "Surya",
+      posisi: "Manager",
+      gaji: 80000,
+    usia: 35,
+    
+    },
+    {
+      nama: "Aji ahmad",
+      posisi: "Admin",
+      gaji: 60000,
+      usia: 28,
+      
+    },
+    {
+      nama: "Bobi husein",
+      posisi: "Produksi",
+      gaji: 65000,
+      usia: 22,
+      departemen: "Teknologi"
+    },
+    {
+      nama: "Elva mauludi",
+      posisi: "Desainer",
+      gaji: 55000,
+      usia: 27,
+      departemen: "Desain"
+    }
+  ];
+  
+  
+  const a = dataKaryawan.map((dataKaryawan) => dataKaryawan.nama);
+  console.log(a);
+  
+  const i = dataKaryawan.filter((dataKaryawan) => dataKaryawan.usia < 30);
+  console.log(i);
+
+  */
+
